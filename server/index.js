22f4e446d635314c74156bed75146a478309f9cf
eyosiyas-js <eyosiyasat@gmail.com>
2023-10-16 07:17:28 -0700

    subscrpitions

--- server/index.js
+++ server/index.js
@@ -5,56 +5,57 @@ const { ApolloServer, gql } = require("apollo-server-express");
 const { PubSub } = require("graphql-subscriptions");
 const { SubscriptionServer } = require("subscriptions-transport-ws");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
-
+const resolvers = require("./graphql/resolvers");
+const typeDefs = require("./graphql/typeDefs");
+const contextMiddleware = require("./utils/contextMiddleware.js");
 (async () => {
-    
-    const pubsub = new PubSub();
-    const app = express();
-    const httpServer = createServer(app);
+  const pubsub = new PubSub();
+  const app = express();
+  const httpServer = createServer(app);
 
-    const typeDefs = gql`
-    type Query {
-      viewMessages: [Message!]
-    }
-    type Mutation {
-      sendMessage(name: String, content: String): Message!
-    }
-    type Subscription {
-      receiveMessage: Message!
-    }
-    type Message {
-        id: ID!
-        name: String!
-        content: String
-    }
-  `;
+  //   const typeDefs = gql`
+  //   type Query {
+  //     viewMessages: [Message!]
+  //   }
+  //   type Mutation {
+  //     sendMessage(name: String, content: String): Message!
+  //   }
+  //   type Subscription {
+  //     receiveMessage: Message!
+  //   }
+  //   type Message {
+  //       id: ID!
+  //       name: String!
+  //       content: String
+  //   }
+  // `;
 
-  let messages = []
-  const resolvers = {
-    Query: {
-      viewMessages() {
-        return messages;
-      },
-    },
-    Mutation: {
-      sendMessage: (parent, { name, content }) => {
-        const id = messages.length;
-        var new_message = {
-            id,
-            name,
-            content
-        }
-        messages.push(new_message);
-        pubsub.publish("MessageService", {receiveMessage: new_message});
-        return new_message;
-      },
-    },
-    Subscription: {
-      receiveMessage: {
-        subscribe: () => pubsub.asyncIterator(["MessageService"]),
-      },
-    },
-  };
+  let messages = [];
+  // const resolvers = {
+  //   Query: {
+  //     viewMessages() {
+  //       return messages;
+  //     },
+  //   },
+  //   Mutation: {
+  //     sendMessage: (parent, { name, content }) => {
+  //       const id = messages.length;
+  //       var new_message = {
+  //           id,
+  //           name,
+  //           content
+  //       }
+  //       messages.push(new_message);
+  //       pubsub.publish("MessageService", {receiveMessage: new_message});
+  //       return new_message;
+  //     },
+  //   },
+  //   Subscription: {
+  //     receiveMessage: {
+  //       subscribe: () => pubsub.asyncIterator(["MessageService"]),
+  //     },
+  //   },
+  // };
 
   const schema = makeExecutableSchema({ typeDefs, resolvers });
 
@@ -66,7 +67,7 @@ const { makeExecutableSchema } = require("@graphql-tools/schema");
 
   SubscriptionServer.create(
     { schema, execute, subscribe },
-    { server: httpServer, path: '/' }
+    { server: httpServer, path: "/" }
   );
   const PORT = 4000;
   httpServer.listen(PORT, () => {
@@ -77,5 +78,4 @@ const { makeExecutableSchema } = require("@graphql-tools/schema");
       `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.graphqlPath}`
     );
   });
-
 })();

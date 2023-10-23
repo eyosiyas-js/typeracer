fcbbe99d8bf393bce65361cb36c6667660411c74
eyosiyas-js <eyosiyasat@gmail.com>
2023-10-23 06:58:48 -0700

    Front_auth

---
+++ client/src/guards/GuestGuard.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import { Navigate } from 'react-router-dom';
+// hooks
+import useAuth from '../hooks/useAuth';
+// routes
+
+
+// ----------------------------------------------------------------------
+
+GuestGuard.propTypes = {
+  children: PropTypes.node
+};
+
+export default function GuestGuard({ children }) {
+  const { isAuthenticated } = useAuth();
+
+  if (isAuthenticated) {
+    return <Navigate to={'/'} />;
+  }
+
+  return <>{children}</>;
+}
---
+++ client/src/routes/index.js
@@ -0,0 +1,77 @@
+import { Suspense, lazy } from "react";
+import { Navigate, useRoutes, useLocation } from "react-router-dom";
+// layouts
+
+// guards
+import GuestGuard from "../guards/GuestGuard";
+import AuthGuard from "../guards/AuthGuard";
+// import RoleBasedGuard from '../guards/RoleBasedGuard';
+// components
+import SignUp from "../pages/auth/SignUp";
+import Login from "../pages/auth/Login";
+
+// ----------------------------------------------------------------------
+
+// const Loadable = (Component) => (props) => {
+//   // eslint-disable-next-line react-hooks/rules-of-hooks
+//   const { pathname } = useLocation();
+//   const isDashboard = pathname.includes("/dashboard");
+
+//   return (
+//     <Suspense
+//       fallback={
+//         <LoadingScreen
+//           sx={{
+//             ...(!isDashboard && {
+//               top: 0,
+//               left: 0,
+//               width: 1,
+//               zIndex: 9999,
+//               position: "fixed",
+//             }),
+//           }}
+//         />
+//       }
+//     >
+//       <Component {...props} />
+//     </Suspense>
+//   );
+// };
+
+export default function Router() {
+  return useRoutes([
+    {
+      path: "auth",
+      children: [
+        {
+          path: "login",
+          element: (
+            <GuestGuard>
+              <Login />
+            </GuestGuard>
+          ),
+        },
+        {
+          path: "register",
+          element: (
+            <GuestGuard>
+              <SignUp />
+            </GuestGuard>
+          ),
+        },
+      ],
+    },
+
+    // Dashboard Routes
+
+    {
+      path: "/",
+      //   element: <MainLayout />,
+      children: [
+        { path: "/", element: <h1>Landing page</h1> },
+ 
+      ],
+    },
+    { path: "*", element: <Navigate to="/404" replace /> },
+  ]);
+}
